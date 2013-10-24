b7922cd2f102fd416e7ed7e984ec01fc76503909
Patrizia Peller <patrizia.peller@googlemail.com>
2013-10-24 21:44:11 +0200

    nouveau graphe montrant les TdU pour les machines: programmation matlab et image

--- graph_personnel.m
+++ graph_personnel.m
@@ -5,13 +5,20 @@ resX=zeros(0);
 
 for i=1:pas
 benef(1,i)=((i)/pas)*benefMax;
-prodTot(1,i) = sum(personnel((i/pas)*benefMax));
+planProd = personnel((i/pas)*benefMax, [26; 15; 0; 5; 10; 35]);
+tempsM1(1, i) = sum(planProd.*[11; 15; 0; 5; 0; 10]);
+tempsM5(1, i) = sum(planProd.*[15; 0; 0; 0; 10; 25]);
+tempsTot(1, i) = tempsM1(1, i) + tempsM5(1, i);
+prodTot(1,i) = sum(planProd);
 
 end
 
-plot(benef,prodTot,'k')
-plot([0.7*benefMax 0.7*benefMax], [0 600]);
-plot([0.9*benefMax 0.9*benefMax], [0 600]);
-plot([5535 5535], [0 600]);
-plot(benef,482.4);
+subplot(1, 2, 1), plot(benef,prodTot,'b', [0.7*benefMax 0.7*benefMax], [0 600], 'k', [5761 5761], [0 600], 'k', [6182 6182], [0 600],'k', benef,482.4, 'r');
+%plot([0.7*benefMax 0.7*benefMax], [0 600], 'b');
+%plot([0.9*benefMax 0.9*benefMax], [0 600], 'b');
+%plot([5535 5535], [0 600],'r');
+%plot(benef,482.4, 'b');
+subplot(1, 2, 2), plot(benef,tempsM1, 'b', benef,tempsM5, 'r', benef, tempsTot, 'g', [0.7*benefMax 0.7*benefMax], [0 10000], 'k', [5761 5761], [0 10000], 'k', [6182 6182], [0 10000],'k'), legend('machine 1','machine 5', 'temps total');
+%plot(benef,tempsM5, 'g');
+
 end
